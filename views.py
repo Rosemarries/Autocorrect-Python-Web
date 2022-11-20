defce3e764a99302c4e637852cf02e6fd951ce2e
Rosemarries <rosemarries.404notfound@gmail.com>
2022-11-20 12:06:32 +0700

    basic Autocorrect

--- views.py
+++ views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, jsonify, redirect, url_for
-import cgi
+import pandas as pd
 import actions
 
 views = Blueprint(__name__, "views")
@@ -8,16 +8,15 @@ views = Blueprint(__name__, "views")
 def home():
     return render_template("index.html")
 
-@views.route("/result", methods = [ "POST", "GET" ])
+@views.route("/", methods = [ "POST", "GET" ])
 def result():
     # need to fix this one!!!
     if request.method == "POST":
-        form = cgi.FieldStorage()
-        word = form.getvalue("word")
+        word = request.form["word"]
         
         word_l = actions.save_vocab()
         probabilities = actions.get_probabilities(actions.get_count(word_l, word))
         calculate = actions.calculate(word, word_l, probabilities)
         if word in word_l:
-            return render_template("result.html", result = f"We have {word} in our dictionary!")
-        return render_template("result.html", result = calculate)
+            return render_template("result.html", tables=[f"We have {word} in our dictionary!"], titles="Result")
+        return render_template("result.html", tables=[calculate.to_html(classes="data")], titles="Result")
